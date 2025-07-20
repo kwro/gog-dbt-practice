51b1221d3dce01863ddacb334c1be5dce3092109
KatarzynaWrona-cnic <katarzyna.wrona@centralnic.com>
2025-07-20 10:40:38 +0200

    analysis

--- gog/models/test.sql
+++ gog/models/test.sql
@@ -2,5 +2,16 @@
     enabled = false
 ) }}
 
-select sum(amount_pln) from public.successful_transactions
-where transaction_date between '2025-07-01' and '2025-07-02'
+ select * from public.marts_reconcile_raw_transactions_vs_psp_transactions
+
+
+select *
+from public_raw.transaction_data t
+left join public_raw.psp_transactions p
+    on t.transaction_id = p.original_transaction_id
+    and t.currency = p.psp_currency
+    and t.amount = p.psp_amount
+where p.original_transaction_id is not null
+
+
+ROLLBACK;
